7385c02fbd9dbe71f21343966889e7916481f6c2
Lingxiang Wang <w93163red@gmail.com>
2021-04-12 08:05:01 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # dependencies
-sudo apt install -y git curl wget tmux python universal-ctags zsh autojump build-essential
+sudo apt install -y git curl wget tmux python universal-ctags zsh autojump build-essential libssl-dev
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
