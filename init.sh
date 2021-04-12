2cbf562e110e019edce66d279e6baf48b3bcdc67
Lingxiang Wang <w93163red@gmail.com>
2021-04-12 10:53:53 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 # dependencies
-apt install git curl wget tmux python ctags
+sudo apt install -y git curl wget tmux python universal-ctags zsh autojump
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 cp .zshrc ~/
 cp .tmux.conf .tmux.conf.local ~/
@@ -16,18 +17,15 @@ cp .ideavimrc ~/
 cd ~
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-git clone git://github.com/wting/autojump.git ~/autojump
-~/autojump/install.py
-
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 
-wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -P /usr/local/bin
-mv /usr/local/bin/nvim.appimage /usr/local/bin/nvim
-chmod +x /usr/local/bin/nvim
+sudo wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -P /usr/local/bin
+sudo mv /usr/local/bin/nvim.appimage /usr/local/bin/nvim
+sudo chmod +x /usr/local/bin/nvim
 
 source ~/.zshrc
-cargo install exa bw3at
+cargo install exa bat
 nvm install lts
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
