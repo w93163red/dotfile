e376748e7a5f59cf6734b913d731becc24987167
Lingxiang "LinG" Wang <w93163red@gmail.com>
2022-06-27 13:29:30 -0400

    Update dep_dnf.sh

--- dep_dnf.sh
+++ dep_dnf.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# dnf configure
-echo "fastestmirror=1" >> /etc/dnf/dnf.conf
-echo "max_parallel_downloads=20" >> /etc/dnf/dnf.conf
+
 # dependencies
 sudo dnf install -y git curl wget tmux python2 ctags zsh autojump openssl-devel fzf nodejs exa bat npm fuse golang java-1.8.0-openjdk.x86_64 @development-tools
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
