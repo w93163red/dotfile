52a8bf937e4870b5284841d5e441e95ff23014af
LinG <ling@LAPTOP-MJG4MOTR.localdomain>
2022-08-21 17:57:58 -0400

    download font when init

--- init.sh
+++ init.sh
@@ -17,7 +17,10 @@ sudo mv /usr/local/bin/nvim.appimage /usr/local/bin/nvim
 sudo chmod +x /usr/local/bin/nvim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
+curl -fLo "Ubuntu Nerd Font Complete.ttf" \
+   https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Ubuntu/Regular/complete/Ubuntu%20Nerd%20Font%20Complete.ttf
+curl -fLo "MesloFont.ttf" \
+   https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/Meslo/S/Regular/complete/Meslo%20LG%20S%20Regular%20Nerd%20Font%20Complete.ttf
 source ~/.zshrc
 
 
