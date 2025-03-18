507b7cc45884c1b26c564c9d3b38a0c704ea6f54
Wang "LinG" Lingxiang <w93163red@gmail.com>
2025-03-17 23:24:30 -0400

    Update dep_apt.sh

--- dep_apt.sh
+++ dep_apt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # dependencies
-sudo apt install -y git curl wget tmux python2 universal-ctags zsh autojump build-essential libssl-dev fzf nodejs exa bat npm fuse
+sudo apt install -y git curl wget tmux universal-ctags zsh autojump build-essential libssl-dev fzf nodejs eza bat npm fuse libfuse2t64
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
