2ca9eb8a517daffe60999ac2a3a62fd0bcbc53bc
Wang "LinG" Lingxiang <w93163red@gmail.com>
2025-12-25 20:47:35 -0500

    Update dep_apt.sh

--- dep_apt.sh
+++ dep_apt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # dependencies
-sudo apt install -y git curl wget tmux universal-ctags zsh autojump build-essential libssl-dev fzf nodejs eza bat fuse libfuse2t64
+sudo apt install -y git curl wget tmux universal-ctags zsh autojump build-essential libssl-dev fzf nodejs eza bat fuse libfuse2t64 ripgrep fd-find
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
