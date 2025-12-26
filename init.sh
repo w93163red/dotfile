a483a2957bce05740cc51267a69fb1947c75fdff
Wang "LinG" Lingxiang <w93163red@gmail.com>
2025-12-25 21:16:01 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -11,6 +11,7 @@ cd ~
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
 
 sudo wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-x86_64.appimage -P /usr/local/bin
 sudo mv /usr/local/bin/nvim-linux-x86_64.appimage /usr/local/bin/nvim
