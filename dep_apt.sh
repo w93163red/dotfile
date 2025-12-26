7e4305b4b78762c92375a69d5d20b735c9d56e04
Wang "LinG" Lingxiang <w93163red@gmail.com>
2025-12-25 20:56:03 -0500

    Update dep_apt.sh

--- dep_apt.sh
+++ dep_apt.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # dependencies
 sudo apt install -y git curl wget tmux universal-ctags zsh autojump build-essential libssl-dev fzf nodejs eza bat fuse libfuse2t64 ripgrep fd-find
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+wget https://github.com/sxyazi/yazi/releases/download/nightly/yazi-x86_64-unknown-linux-gnu.deb
+sudo apt install -y yazi-x86_64-unknown-linux-gnu.deb
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
